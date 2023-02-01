e44cec83812ce3f546e2c6268a1c846a94ac3096
AlbertHuang <r.albert.huang@gmail.com>
2023-02-01 17:22:56 +0800

    chore: Add appId to consul default checks
    fix https://github.com/dapr/components-contrib/issues/2489
    
    Signed-off-by: AlbertHuang <r.albert.huang@gmail.com>

--- nameresolution/consul/consul.go
+++ nameresolution/consul/consul.go
@@ -253,7 +253,7 @@ func getRegistrationConfig(cfg configSpec, props map[string]string) (*consul.Age
 				Name:     "Dapr Health Status",
 				CheckID:  fmt.Sprintf("daprHealth:%s", id),
 				Interval: "1s",
-				HTTP:     fmt.Sprintf("http://%s/v1.0/healthz", net.JoinHostPort(host, httpPort)),
+				HTTP:     fmt.Sprintf("http://%s/v1.0/healthz/%s", net.JoinHostPort(host, httpPort), appID),
 			},
 		}
 	}
